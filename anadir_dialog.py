bd80778610cb97d9d02be36bb48cd2aa23a1c656
ger-ator <gerator@gmail.com>
2016-04-12 16:32:47 +0200

    Added files via upload
    Eliminado sqltablemodel, uso sqlquerymodel

--- anadir_dialog.py
+++ anadir_dialog.py
@@ -19,29 +19,22 @@ class AnadirDialog(QtWidgets.QDialog, Ui_Dialog):
                                    'Otros'])
 
         ##Configuracion del origen de datos
-        self.model = QtSql.QSqlTableModel(self)
-        self.model.setTable("personal")
-        self.model.select()
-        self.resultado_view.setModel(self.model)
-        self.resultado_view.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+        self.model = QtSql.QSqlQueryModel(self)
+        self.populate_model()
+        self.proxy_model = QtCore.QSortFilterProxyModel(self)
+        self.proxy_model.setSourceModel(self.model)    
+        self.resultado_view.setModel(self.proxy_model)
         self.sel_model = self.resultado_view.selectionModel()
-        self.encabezado = self.resultado_view.horizontalHeader()
         ####
         ####Configuracion visual de la tabla
-        self.resultado_view.hideColumn(self.model.fieldIndex("personal_id"))
-        self.resultado_view.hideColumn(self.model.fieldIndex("unidad"))
-        self.model.setHeaderData(self.model.fieldIndex("siglas"),
-                                 QtCore.Qt.Horizontal, "Siglas")
-        self.model.setHeaderData(self.model.fieldIndex("nombre"),
-                                 QtCore.Qt.Horizontal, "Nombre")
-        self.model.setHeaderData(self.model.fieldIndex("apellido1"),
-                                 QtCore.Qt.Horizontal, "Primer Apellido")
-        self.model.setHeaderData(self.model.fieldIndex("apellido2"),
-                                 QtCore.Qt.Horizontal, "Segundo Apellido")
-        self.model.setHeaderData(self.model.fieldIndex("puesto"),
-                                 QtCore.Qt.Horizontal, "Puesto")
-        self.model.setHeaderData(self.model.fieldIndex("equipo"),
-                                 QtCore.Qt.Horizontal, "Equipo")
+        self.model.setHeaderData(0, QtCore.Qt.Horizontal, "Siglas")
+        self.model.setHeaderData(1, QtCore.Qt.Horizontal, "Nombre")
+        self.model.setHeaderData(2, QtCore.Qt.Horizontal, "Primer Apellido")
+        self.model.setHeaderData(3, QtCore.Qt.Horizontal, "Segundo Apellido")
+        self.model.setHeaderData(4, QtCore.Qt.Horizontal, "Puesto")
+        self.resultado_view.hideColumn(5)##unidad
+        self.model.setHeaderData(6, QtCore.Qt.Horizontal, "Equipo")
+        self.resultado_view.hideColumn(7) ##personal_id
         self.resultado_view.resizeColumnsToContents()
         ####
         ##Inhabilito OK en buttonbox
@@ -49,15 +42,23 @@ class AnadirDialog(QtWidgets.QDialog, Ui_Dialog):
         ####
         ##Asignacion de eventos
         self.sel_model.selectionChanged.connect(self.seleccionCambiada)
-        self.buscar_ledit.textEdited.connect(self.buscar_text_edited)
+        self.buscar_ledit.textEdited.connect(self.filtro_text_edited)
+        self.filtro_cbox.currentIndexChanged.connect(self.proxy_model.setFilterKeyColumn)
         self.buttonBox.accepted.connect(self.buttonBox_OK)
-        self.encabezado.sectionClicked.connect(self.encabezado_clicked)
         ####
 
-    def buscar_text_edited(self):
-        filtro = "{0} LIKE '{1}%'".format(self.filtro_cbox.currentText().lower(),
-                                          self.buscar_ledit.text())
-        self.model.setFilter(filtro)
+    def populate_model(self):
+        self.model.setQuery("SELECT siglas, nombre, apellido1, "
+                            "apellido2, puesto, unidad, equipo, "
+                            "personal_id "
+                            "FROM personal")
+        self.resultado_view.resizeColumnsToContents()
+
+    def filtro_text_edited(self, texto):
+        filtro = QtCore.QRegExp("^{0}".format(texto),
+                                QtCore.Qt.CaseInsensitive,
+                                QtCore.QRegExp.RegExp)
+        self.proxy_model.setFilterRegExp(filtro)
 
     def buttonBox_OK(self):
         if self.inicio_dedit.date() > self.final_dedit.date():
@@ -73,17 +74,13 @@ class AnadirDialog(QtWidgets.QDialog, Ui_Dialog):
                       self.motivo_cbox.currentText())
             self.accept()
 
-    def encabezado_clicked(self):
-        self.buttonBox.button(QtWidgets.QDialogButtonBox.Ok).setEnabled(False)
-
     def seleccionCambiada(self, selected, deselected):
         if selected.isEmpty():
             self.buttonBox.button(QtWidgets.QDialogButtonBox.Ok).setEnabled(False)
         else:
             self.buttonBox.button(QtWidgets.QDialogButtonBox.Ok).setEnabled(True)
             indices = selected.indexes()
-            self.trabajador_id = indices[0].sibling(indices[0].row(),
-                                                    self.model.fieldIndex("personal_id"))
+            self.trabajador_id = indices[0].sibling(indices[0].row(), 7)##personal_id
             
 #######################################################################################
 if __name__ == '__main__':
